ca4def085f0f1d9c6daf7d2a54e19497d41d7439
Aiker Ary Acosta Cantillo <aikiringo26@gmail.com>
2023-11-19 22:22:55 -0500

    Fixing some styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,12 +4,11 @@ import './Header.css';
 const Header = () => {
     return (
         <header className="header">
-            {/* Picture */}
             <picture>
-                <img src="/pestel.svg" alt="Header" />
+                <img src="/pestel.svg" alt="Pestel logo" className="logo" />
             </picture>
             <nav className="header_nav">
-                <ul className="header__nav-list">
+                <ul className="header_nav-list">
                     <li className="nav-item">Home</li>
                     <li className="nav-item">About</li>
                     <li className="nav-item">Contact</li>
